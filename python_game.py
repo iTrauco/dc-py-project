486cc5586f4252a67c6131c5b43b1989217f17b2
iTrauco <Chris@Trau.co>
2019-02-22 10:55:38 -0500

    Hero class test

---
+++ python_game.py
@@ -0,0 +1,6 @@
+# establishes hero/goblin gclass
+class Hero():    
+    def __init__(self, name, power, health):  
+        self.name = name
+        self.power = power #apply to all hero instances
+        self.health = health #pass health and power top left
---
+++ tests.py
@@ -0,0 +1,46 @@
+# make a hero class to store the health and power 
+from python_game import Hero
+
+thor = Hero('Thor', 10, 10)
+print(thor.name) 
+print(thor.health)
+print(thor.power)# to print attributes, to print it in name add another attribute
+# make a goblin class to store the health and power
+
+flash = Hero('Barry Allen', 10, 10)
+print(flash.name)
+print(flash.health)
+print(thor.power)
+
+#################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
